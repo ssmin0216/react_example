73218e68d35aafe7aaa56fd64a2a48aa266f5c35
ssmin0216 <ssmin0216@naver.com>
2023-05-03 00:10:24 +0900

    [diary] 일기 데이터 수정
    배열 데이터 수정

--- src/App.js
+++ src/App.js
@@ -22,15 +22,23 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
-    const newDiaryList = data.filter((it)=> it.id !== targetId);
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
+  }
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? {...it, content: newContent} : it
+      )
+    );
   }
 
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
-      <DiaryList diaryList={data} onDelete={onDelete}/>
+      <DiaryList diaryList={data} onEdit={onEdit} onRemove={onRemove}/>
     </div>
   );
 };
