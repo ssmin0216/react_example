bdfe57a0c188d763ab79dc539cf6c131dadf79ec
ssmin0216 <ssmin0216@naver.com>
2023-05-02 23:43:57 +0900

    [diary] 일기 데이터 삭제
    배열 데이터 삭제

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,11 @@
-const DiaryItem = ({author, content, created_date, emotion, id}) => {
+const DiaryItem = ({
+   onDelete,
+   author,
+   content,
+   created_date,
+   emotion,
+   id
+ }) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -8,7 +15,16 @@ const DiaryItem = ({author, content, created_date, emotion, id}) => {
         <br/>
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">일기 : {content}</div>
+      <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)){
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
