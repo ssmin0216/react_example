c21bfd04fada33507ae0d1afb30e7387d1290f6c
ssmin0216 <ssmin0216@naver.com>
2023-05-04 00:50:19 +0900

    [exam1] Lifecycle 2
    React의 Lifecycle 2 useEffect mount, update, unmount

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,44 +1,28 @@
 import React, {useEffect, useState} from "react";
 
-const Lifecycle = () => {
-
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
+const UnmountTest = () => {
 
   useEffect(() => {
     console.log("mount");
+    return () => {
+      // Unmount 시점에 실행
+      console.log("Unmount");
+    };
   }, []);
+  return <div>Unmount Testing Component</div>
+}
 
-  useEffect(() => {
-    console.log("update");
-  });
+const Lifecycle = () => {
 
-  useEffect(() => {
-    console.log(`count is update : ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다.");
-      setCount(1);
-    }
-  }, [count]);
-
-  useEffect(() => {
-    console.log(`text is update : ${text}`);
-  }, [text]);
-
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{padding: 20}}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)}/>
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest/>}
     </div>
-
-  )
-    ;
+  );
 };
 
 export default Lifecycle;
