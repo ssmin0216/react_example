0e1f3b8aeb2570f800b04912f6f202de0ece8e37
ssmin0216 <ssmin0216@naver.com>
2023-05-23 23:56:49 +0900

    [sample] react memo2 areEqual
    컴포넌트 최적화 객체는 얕은복사이므로 areEqual로 비교

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,36 +1,49 @@
 import React, {useState, useEffect} from 'react';
 
-const TextView = React.memo(({text}) => {
+const CounterA = React.memo(({count}) => {
   useEffect(() => {
-    console.log(`Update text : ${text}`);
+    console.log(`Counter A Update: ${count}`);
   });
-  return <div>{text}</div>
+  return <div>{count}</div>;
 });
 
-const CountView = React.memo(({count}) => {
+const CounterB = ({obj}) => {
   useEffect(() => {
-    console.log(`Update count : ${count}`);
+    console.log(`Counter B Update : ${obj.count}`);
   });
-  return <div>{count}</div>
-});
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.count === nextProps.count;
+}
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
 
 const OptimizeTest = () => {
 
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1
+  });
 
-  return <div style={{padding: 50}}>
-    <div>
-      <h2>count</h2>
-      <CountView count={count}/>
-      <button onClick={() => setCount(count + 1)}>+</button>
+  return (
+    <div style={{padding: 50}}>
+      <div>
+        <h2>Counter A</h2>
+        <CounterA count={count}/>
+        <button onClick={() => setCount(count)}>A button</button>
+      </div>
+      <div>
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj}/>
+        <button onClick={() => setObj({
+          count: obj.count
+        })}>B button
+        </button>
+      </div>
     </div>
-    <div>
-      <h2>text</h2>
-      <TextView text={text}/>
-      <input value={text} onChange={(e) => setText(e.target.value)}/>
-    </div>
-  </div>
+  );
 }
 
 export default OptimizeTest;
